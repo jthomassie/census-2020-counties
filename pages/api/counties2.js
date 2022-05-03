a86f3a28ece8de6b58a506f26e692f080e7d320e
Juan Thomassie <jthomassie1@gmail.com>
2022-05-03 14:11:25 -0400

    u

--- pages/api/counties2.js
+++ pages/api/counties2.js
@@ -1,8 +1,25 @@
 // pages/api/counties2.js
 
 // import { connectToDatabase } from "../lib/mongodb";
-import { getCounties } from "../lib/mongodb";
+import { getCounties } from "../../lib/counties";
 
 export default async function handler(req, res) {
   res.status(200).json(await getCounties());
+}
+
+export async function getServerSideProps() {
+  const { db } = await connectToDatabase();
+
+  const counties = await db
+    .collection("counties")
+    .find({})
+    // .sort({ metacritic: -1 })
+    .limit(20)
+    .toArray();
+
+  return {
+    props: {
+      counties: JSON.parse(JSON.stringify(counties)),
+    },
+  };
 }
