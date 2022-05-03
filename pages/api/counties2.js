edd1e17ea99851e90e9d616ea6350b7e9ec3a227
Juan Thomassie <jthomassie1@gmail.com>
2022-05-03 13:57:09 -0400

    u

--- pages/api/counties2.js
+++ pages/api/counties2.js
@@ -1,6 +1,7 @@
 // pages/api/counties2.js
 
-import { getCounties } from "../../lib/counties";
+// import { connectToDatabase } from "../lib/mongodb";
+import { getCounties } from "../lib/mongodb";
 
 export default async function handler(req, res) {
   res.status(200).json(await getCounties());
