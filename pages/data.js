314e1a3b9e3c22b8332e78d6738d03c4c1746375
Juan Thomassie <jthomassie1@gmail.com>
2022-05-03 16:45:37 -0400

    page updates

--- pages/data.js
+++ pages/data.js
@@ -5,16 +5,26 @@ import useSWR from "swr";
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 const Profile = () => {
-  const { data, error } = useSWR("/api/hello", fetcher);
+  const { data, error } = useSWR("/api/median-age", fetcher);
 
   if (error) return <div>Failed to load</div>;
   if (!data) return <div>Loading...</div>;
+  console.log("data", data);
 
+  //
   return (
-    <div>
-      <h1>{data.name}</h1>
-      <p>{data.bio}</p>
-    </div>
+    <>
+      <h3>Top Counties</h3>
+      <p>median-age</p>
+      <ul>
+        {counties.map((d, i) => (
+          <li>
+            <h6>{d.COUNTY}</h6>
+            <p>{d.POP}</p>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
