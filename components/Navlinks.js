725bd4915e2cda35dbe6212a07dde09ede83d639
Juan Thomassie <jthomassie1@gmail.com>
2022-05-04 16:46:35 -0400

    add more api endpoints to navlinks

--- components/Navlinks.js
+++ components/Navlinks.js
@@ -8,27 +8,6 @@ const Navlinks = ({ active }) => {
   return (
     <div className="px-3 py-2 gray rounded-3">
       <div className="mb-3">
-        <h3 className="m-0">api</h3>
-        <ul className="list-unstyled">
-          <li>
-            <Link href="/api/median-age">
-              <a>median-age</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/api/population">
-              <a>population</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/api/subtypes">
-              <a>subtypes</a>
-            </Link>
-          </li>
-        </ul>
-      </div>
-
-      <div className="mb-3">
         <h3 className="m-0">pages</h3>
         <ul className="list-unstyled">
           <li>
@@ -71,6 +50,37 @@ const Navlinks = ({ active }) => {
               <a>med-age</a>
             </Link>
           </li> */}
+        </ul>
+      </div>
+
+      <div className="mb-3">
+        <h3 className="m-0">api</h3>
+        <ul className="list-unstyled">
+          <li>
+            <Link href="/api/median-age">
+              <a>median-age</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/api/population">
+              <a>population</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/api/subtypes">
+              <a>subtypes</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/api/ra-features">
+              <a>ra-features</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/api/inregion">
+              <a>inregion</a>
+            </Link>
+          </li>
         </ul>
       </div>
     </div>
